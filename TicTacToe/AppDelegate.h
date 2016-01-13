9bc4f56bcaebe48c85fe57ccb57486c32c5e2bb4
Matt Deuschle <mdeuschle@gmail.com>
2016-01-12 21:39:14 -0600

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Matt Deuschle on 1/12/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Matt Deuschle on 1/12/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
