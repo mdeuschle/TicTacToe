2f610d32f9376cf3fa4148cae8670482aa0ec1bc
Kessica Kesington <kkesington@Hogwarts-2.local>
2016-01-14 14:47:29 -0600

    Buttons and constraints

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -19,9 +19,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onButtonLeftTopTapped:(id)sender {
 }
+
+
 
 @end
